fb8a4da18dbf04e03f07c7ab01a01a333fc20b7d
song-chaeyoung <153435389+song-chaeyoung@users.noreply.github.com>
2025-04-05 23:54:16 +0900

    style:tailwind 컬러 변수 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="w-[200px] flex gap-2 items-center ml-1 mt-1">
-      메인페이지 입니다.
-      <CustomIcon icon="NEW_SVG" className="w-[32px] h-[32px]" /> {/* 예시 */}
+    <div className="flex-center h-screen bg-fillGrayDefault ">
+      <div className="w-[200px] flex gap-2 items-center ml-1 mt-1 text-fillPrimaryFocused ">
+        메인페이지 입니다.
+        <CustomIcon icon="NEW_SVG" className="w-[32px] h-[32px]" /> {/* 예시 */}
+      </div>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,42 @@ export default {
         fadeOut: "fadeOut 0.5s ease-in-out 2.5s forwards",
         spin: "spin 0.5s cubic-bezier(0.42, 0, 0.58, 1)",
       },
+      colors: {
+        fgPrimaryDefault: "var(--gray-0)",
+        fgPrimaryHovered: "var(--gray-0)",
+        fgPrimaryFocused: "var(--gray-200)",
+        fgPrimaryPressed: "var(--gray-200)",
+        fgPrimaryDisabled: "var(--gray-200)",
+        fgPrimaryAccent: "var(--primary-100)",
+
+        fgGrayDefault: "var(--gray-50)",
+        fgGrayHovered: "var(--gray-0)",
+        fgGrayFocused: "var(--gray-0)",
+        fgGrayEntered: "var(--gray-0)",
+        fgGrayPressed: "var(--gray-300)",
+        fgGrayDisabled: "var(--gray-300)",
+        fgGrayPlaceholder: "var(--gray-300)",
+
+        bgGrayDepth1: "var(--gray-800)",
+        bgGrayDepth2: "var(--gray-700)",
+        bgGrayDepth3: "var(--gray-600)",
+
+        borderDefault: "var(--gray-300)",
+        borderFocused: "var(--gray-200)",
+        borderDivider: "var(--gray-600)",
+
+        fillPrimaryDefault: "var(--primary-300)",
+        fillPrimaryHovered: "var(--primary-400)",
+        fillPrimaryFocused: "var(--primary-400)",
+        fillPrimaryPressed: "var(--primary-500)",
+        fillPrimaryDisabled: "var(--primary-500)",
+
+        fillGrayDefault: "var(--gray-500)",
+        fillGrayHovered: "var(--gray-400)",
+        fillGrayFocused: "var(--gray-400)",
+        fillGrayPressed: "var(--gray-600)",
+        fillGrayDisabled: "var(--gray-600)",
+      },
     },
   },
 } satisfies Config;
