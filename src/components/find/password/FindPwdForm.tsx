dd08dc76685c64d466367a46ca83fe620f2e9dad
wlrnjs <tjwlrnjs7336@icloud.com>
2025-04-14 05:52:48 +0900

    feat:오토포커스추가

--- src/components/find/password/FindPwdForm.tsx
+++ src/components/find/password/FindPwdForm.tsx
@@ -74,6 +74,7 @@ const FindPwdForm = ({ onSuccess }: FindPwdFormProps) => {
           <input
             id="name"
             type="text"
+            autoFocus
             {...register("name", {
               required: "이름을 입력해주세요.",
             })}
